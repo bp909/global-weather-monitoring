fd4f15cd68f64200a1988be23604cd8fa692ae74
Ben <Ben@DESKTOP-8187TP4.home>
2020-03-24 22:07:28 +0000

    NDVItest working now, sequential operation to sort later

--- NDVItest.m
+++ NDVItest.m
@@ -1,11 +1,23 @@
 [redfil,redpat] = uigetfile('*','Choose a Redclearpic');
 [grefil,grepat] = uigetfile('*','Choose a Greclearpic');
-[blufil,blupat] = uigetfile('*','Choose a Bluclearpic');
+
 redname = fullfile(redpat,redfil);
 redchan = imread(redname);
-bluname = fullfile(blupat,blufil);
-bluchan = imread(bluname);
+
 grename = fullfile(grepat,grefil);
 grechan = imread(grename);
 
-NDVIimg
+NDVIimg = abs((double(grechan-redchan))./(double(redchan+grechan)));
+
+figure;
+imshow(NDimg)
+figure;
+imshow(VIimg)
+figure;
+imshow(NDVIimg)
+figure;
+imground = round(NDVIimg,1);
+imshow(imground)
+NDVIimg1(NDVIimg~=0) = 1;
+NDVIimg1 = NDVIimg~=0;
+imshow(NDVIimg1)
