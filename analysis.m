cdebdfa8f49cf7b46c91f981fa0054876237324e
brain <brain@192.168.1.66>
2020-04-20 15:30:57 +0100

    analysis

---
+++ analysis.m
@@ -0,0 +1,12 @@
+percentcovtrack = zeros(1, modecount);
+NDVIimgs = zeros([imagesiz_Y imagesiz_X modecount]);
+for m = 1:modecount
+    [percentcovtrack(m), NDVIimgs(:,:,m)] = NDVIcalc(colcfi_R(:,:,m),colcfi_G(:,:,m),chan_L);
+end
+
+
+figure;
+plot(percentcovtrack)
+hold on;
+ff = movmean(percentcovtrack,3);
+plot(ff)
