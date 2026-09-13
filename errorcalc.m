a8b0e6ccb6748c2e7edccbf60964b52d1ef700fb
bp909 <60472890+bp909@users.noreply.github.com>
2026-09-13 12:33:15 +0100

    Added comments

--- errorcalc.m
+++ errorcalc.m
@@ -1,4 +1,4 @@
-% this calculates the error on the vegetation position
+% this calculates the error on the vegetation position across the time
 PosSpr = readmatrix('E:\OneDrive\OneDrive - The University of Nottingham\3rd Year Project\recourses\Summary data\PosAndSpre1.csv');
 
 PosSpr(isnan(PosSpr))=0;
