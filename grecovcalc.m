49efcaa9eddc4a2577b83e2fbf7f079b34cc13b2
bp909 <60472890+bp909@users.noreply.github.com>
2026-09-13 11:39:49 +0100

    Enhance grecovcalc function with a description
    Added a comment to describe the function's purpose.

--- grecovcalc.m
+++ grecovcalc.m
@@ -1,5 +1,6 @@
 function landcoverage = grecovcalc(NDVIimg,landmask)
 %landcoverage = grecovcalc(NDVIimg,landmask)
+    % Function calculating average NDVI in landmasked area
     NDVIimg = double(NDVIimg);
     NDVIimg(landmask~=0) = NaN;
     landcoverage = sum(NDVIimg,'all','omitnan')./sum(~landmask,'all');
